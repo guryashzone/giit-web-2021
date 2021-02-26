51f48bc9cc188d549e14f597b08642f830983629
Guryash Singh <46774163+guryashzone@users.noreply.github.com>
2021-02-26 17:35:35 +0530

    Add files via upload

--- login-module/display.php
+++ login-module/display.php
@@ -1,4 +1,33 @@
-<?php  require_once('includes/connection.php'); ?>
+<?php require_once('includes/connection.php'); ?>
+<?php 
+	$error = $success = null;
+	if (isset($_POST['addBtn'])) {
+		
+		$empno = $_POST['empno'];
+		$empname = $_POST['empname'];
+		$empdept = $_POST['empdept'];
+		$empstatus = $_POST['empstatus'];
+
+		$query = "INSERT INTO `employee` VALUES (null, '$empno', '$empname', $empdept, '$empstatus')";
+		$res = mysqli_query($conn, $query);
+		$err = mysqli_error($conn);
+
+		if ($err) {
+			$error = $err;
+		} else {
+			$success = true;
+		}
+
+	}
+
+
+	$query = "SELECT `employee_number` FROM `employee` ORDER BY `employee_id` DESC LIMIT 1";
+	$res = mysqli_query($conn, $query);
+	$row = mysqli_fetch_object($res);
+	$new_empno = $row->employee_number;
+	$new_empno = ++$new_empno;
+
+ ?>
 <!DOCTYPE html>
 <html lang="en">
 <head>
@@ -9,8 +38,55 @@
 
 
 	<div class="row mt-5">
-		<div class="col-lg-3"><br></div>
-		<div class="col-lg-6">
+		<div class="col-lg-4">
+			<div class="bg-primary p-2 font-weight-bold text-center text-white shadow">
+				Add Employee
+			</div>
+			<div class="p-3 border">
+
+				<?php 
+					if ($error != null) {
+						echo "<div class='alert alert-danger'>$error</div>";
+					} 
+					if ($success == true) {
+						echo "<div class='alert alert-success'>Employee added successfully!</div>";
+					}
+				 ?>
+
+
+				<form action="" method="POST">
+					
+					<label>EMP NO</label>
+					<input type="text" name="empno" class="form-control form-control-sm rounded-0 mb-2" placeholder="Enter employee number" value="<?php echo $new_empno; ?>" readonly>
+
+
+					<label>EMP NAME</label>
+					<input type="text" name="empname" class="form-control form-control-sm rounded-0 mb-2" placeholder="Enter employee name">
+
+					<label>EMP DEPT</label>
+					<select name="empdept" class="form-control form-control-sm rounded-0 mb-2">
+						<option value selected disabled>--Select Department--</option>
+						<?php 
+							$query = "SELECT * FROM `employee_department_master` WHERE `dept_status`='active' ORDER BY `dept_name`";
+							$res = mysqli_query($conn, $query);
+							while ($row = mysqli_fetch_object($res)) {
+								echo "<option value='$row->dept_id'>$row->dept_name</option>";
+							}
+						 ?>
+					</select>
+					
+					<label>EMP STATUS</label>
+					<select name="empstatus" class="form-control form-control-sm rounded-0 mb-2">
+						<option value selected disabled>--Select Status--</option>
+						<option value="active">Active</option>
+						<option value="inactive">Inactive</option>
+					</select>
+
+					<button type="submit" name="addBtn" class="btn btn-primary btn-sm">+ Add</button>
+				</form>
+			</div>
+		</div>
+		<div class="col-lg-7">
 			<div class="bg-success p-2 font-weight-bold text-center text-white shadow">
 				Employee details
 			</div>
@@ -32,7 +108,9 @@
 						WHERE
 							`employee_department_master`.`dept_id` = `employee`.`employee_department`
 						ORDER BY
-							`employee_id`";
+							`employee_id`
+						LIMIT 10
+						";
 
 
 					$res = mysqli_query($conn, $query);
@@ -62,8 +140,7 @@
 
 				 ?>
 			</table>
-		</div>	
-		<div class="col-lg-3"><br></div>
+		</div>
 	</div>
 
 
